5ae053b560faffc1eaafa42b81dd43864bf67cf1
Nick DiMola <nick@sdsolutions.io>
2023-01-18 13:15:08 -0500

    v3.0.1

--- src/main/java/io/sdsolutions/particle/core/config/MasterApplicationConfig.java
+++ src/main/java/io/sdsolutions/particle/core/config/MasterApplicationConfig.java
@@ -1,18 +1,11 @@
 package io.sdsolutions.particle.core.config;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.*;
 
 import jakarta.servlet.MultipartConfigElement;
 
 import io.sdsolutions.particle.core.interceptor.JsonHijackingInterceptor;
-import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +16,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This is a master configuration that makes it easy to get off the ground with a new application.
  * Rather than writing your own, you can merely extend this one to get the basic stuff you'll need to
@@ -32,8 +29,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Your extended AppConfig can house anything else you need to run your app.
  */
 public class MasterApplicationConfig implements WebMvcConfigurer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MasterApplicationConfig.class);
 
     @Autowired
     private JsonHijackingInterceptor jsonHijackingInterceptor;
@@ -66,59 +61,6 @@ public class MasterApplicationConfig implements WebMvcConfigurer {
 
     protected List<Converter<?, ?>> getModelMapperConverters() {
         return Collections.emptyList();
-    }
-
-    protected Map<String, Converter<?, ?>> availableModelMapperConverters() {
-        AbstractConverter<String, Boolean> stringBooleanConverter = new AbstractConverter<>() {
-            @Override
-            protected Boolean convert(String source) {
-                if (source != null) {
-                    if (source.toLowerCase().startsWith("y")) {
-                        return Boolean.TRUE;
-                    } else if (source.toLowerCase().startsWith("n")) {
-                        return Boolean.FALSE;
-                    }
-                }
-                return Boolean.TRUE;
-            }
-        };
-
-        AbstractConverter<Boolean, String> booleanStringConverter = new AbstractConverter<>() {
-            @Override
-            protected String convert(Boolean source) {
-                return source != null && (source) ? "Yes" : "No";
-            }
-        };
-
-        AbstractConverter<LocalDateTime, Date> localDateTimeDateConverter = new AbstractConverter<>() {
-            @Override
-            protected Date convert(LocalDateTime source) {
-                if (source == null) {
-                    return null;
-                }
-
-                return Date.from(source.toInstant(ZoneOffset.UTC));
-            }
-        };
-
-        AbstractConverter<Date, LocalDateTime> dateLocalDateTimeConverter = new AbstractConverter<>() {
-            @Override
-            protected LocalDateTime convert(Date source) {
-                if (source == null) {
-                    return null;
-                }
-
-                return Instant.ofEpochMilli(source.getTime()).atOffset(ZoneOffset.UTC).toLocalDateTime();
-            }
-        };
-
-        Map<String, Converter<?, ?>> converters = new HashMap<>();
-        converters.put("stringBoolean", stringBooleanConverter);
-        converters.put("booleanString", booleanStringConverter);
-        converters.put("localDateTimeDate", localDateTimeDateConverter);
-        converters.put("dateLocalDateTime", dateLocalDateTimeConverter);
-
-        return converters;
     }
 
     @Override
---
+++ src/main/java/io/sdsolutions/particle/core/dozer/MapperConverters.java
@@ -0,0 +1,73 @@
+package io.sdsolutions.particle.core.dozer;
+
+import org.modelmapper.AbstractConverter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class MapperConverters {
+
+    public static AbstractConverter<String, Boolean> STRING_BOOLEAN = new AbstractConverter<>() {
+        @Override
+        protected Boolean convert(String source) {
+            if (source != null) {
+                if (source.toLowerCase().startsWith("y")) {
+                    return Boolean.TRUE;
+                } else if (source.toLowerCase().startsWith("n")) {
+                    return Boolean.FALSE;
+                }
+            }
+            return Boolean.TRUE;
+        }
+    };
+
+    public static AbstractConverter<Boolean, String> BOOLEAN_STRING = new AbstractConverter<>() {
+        @Override
+        protected String convert(Boolean source) {
+            return source != null && (source) ? "Yes" : "No";
+        }
+    };
+
+    public static AbstractConverter<LocalDateTime, Date> LOCALDATETIME_DATE = new AbstractConverter<>() {
+        @Override
+        protected Date convert(LocalDateTime source) {
+            if (source == null) {
+                return null;
+            }
+
+            return Date.from(source.toInstant(ZoneOffset.UTC));
+        }
+    };
+
+    public static AbstractConverter<Date, LocalDateTime> DATE_LOCALDATETIME = new AbstractConverter<>() {
+        @Override
+        protected LocalDateTime convert(Date source) {
+            if (source == null) {
+                return null;
+            }
+
+            return Instant.ofEpochMilli(source.getTime()).atOffset(ZoneOffset.UTC).toLocalDateTime();
+        }
+    };
+
+    public static AbstractConverter<String, String> TRIM_STRING = new AbstractConverter<>() {
+        @Override
+        protected String convert(String source) {
+            return source != null ? source.trim() : null;
+        }
+    };
+
+    public static AbstractConverter<Boolean, Byte> BOOLEAN_BYTE = new AbstractConverter<>() {
+        @Override
+        protected Byte convert(Boolean source) {
+            if (source == null) {
+                return 0;
+            }
+
+            return (byte) (source ? 1 : 0);
+        }
+    };
+
+}
