d70f70cccc279de3bf39b5a7fcd5ec1f81a1aff3
Nick DiMola <nick@sdsolutions.io>
2023-01-18 13:13:27 -0500

    v3.0.1

--- src/main/java/io/sdsolutions/particle/security/config/impl/PassThroughSecurityConfiguration.java
+++ src/main/java/io/sdsolutions/particle/security/config/impl/PassThroughSecurityConfiguration.java
@@ -4,12 +4,8 @@ import io.sdsolutions.particle.security.config.CorsConfigurationProperties;
 import io.sdsolutions.particle.security.filter.AutoLoginFilter;
 import io.sdsolutions.particle.security.config.SecurityConfiguration;
 import io.sdsolutions.particle.security.filter.impl.PassThroughSecurityFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
@@ -21,9 +17,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  *
  */
 public class PassThroughSecurityConfiguration extends SecurityConfiguration {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(PassThroughSecurityConfiguration.class);
-
 
 	public PassThroughSecurityConfiguration(
 			UserDetailsService userDetailsService,
