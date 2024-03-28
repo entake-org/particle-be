77f98518cb6d24d0161317d82884596af96022e7
Nick DiMola <nick@sdsolutions.io>
2024-03-28 16:33:20 -0400

    v3.2.4
    Spring Boot Upgrade + Rest Template changed to use proper project specific objectmapper

--- src/main/java/io/sdsolutions/particle/core/config/MasterApplicationConfig.java
+++ src/main/java/io/sdsolutions/particle/core/config/MasterApplicationConfig.java
@@ -16,6 +16,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -48,8 +50,25 @@ public class MasterApplicationConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        //find and replace Jackson message converter with our own
+        for (int i = 0; i < restTemplate.getMessageConverters().size(); i++) {
+            final HttpMessageConverter<?> httpMessageConverter = restTemplate.getMessageConverters().get(i);
+            if (httpMessageConverter instanceof MappingJackson2HttpMessageConverter){
+                restTemplate.getMessageConverters().set(i, mappingJackson2HttpMessageConverter);
+            }
+        }
+
+        return restTemplate;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
     }
 
     @Bean
