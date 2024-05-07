df04c093238139a05e73cfa710ce62bd9726d9e6
Nick DiMola <nick@sdsolutions.io>
2024-05-06 23:08:07 -0400

    v3.2.5
    Fix for security filter placement

--- src/main/java/io/sdsolutions/particle/security/config/SecurityConfiguration.java
+++ src/main/java/io/sdsolutions/particle/security/config/SecurityConfiguration.java
@@ -13,9 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -73,7 +73,7 @@ public class SecurityConfiguration {
 
 		http.cors(Customizer.withDefaults());
 
-		http.addFilterAfter(autoLoginFilter, SecurityContextHolderAwareRequestFilter.class);
+		http.addFilterBefore(autoLoginFilter, AuthorizationFilter.class);
 
 		configureFilterChain(http);
 
